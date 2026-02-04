726ba4d115c11469ace787f8f9f5ac689116acd3
lilicantona16-design <lilicantona16@gmail.com>
2026-02-04 23:03:51 +0100

    Connexion Supabase

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("L'application Afriquiz est prête !");
+// --- CONFIGURATION SUPABASE ---
+// REMPLACE LES TEXTES CI-DESSOUS PAR TES VRAIES CLÉS
+const SUPABASE_URL = "https://cjxbsrudyqumeuvedozo.supabase.co";
+const SUPABASE_KEY = "sb_publishable_30ieuDVyx_XK30YyvrIFCA_w244ofio
+
+";
+
+// Fonction pour récupérer les questions depuis la base de données
+async function startQuiz(category) {
+    console.log("Chargement du quiz : " + category);
+    
+    // On appelle Supabase
+    const response = await fetch(`${SUPABASE_URL}/rest/v1/questions?category=eq.${category}`, {
+        headers: {
+            "apikey": SUPABASE_KEY,
+            "Authorization": `Bearer ${SUPABASE_KEY}`
+        }
+    });
+
+    const questions = await response.json();
+
+    if (questions.length > 0) {
+        // Pour l'instant, on affiche la première question dans une alerte pour tester
+        alert("Première question : " + questions[0].question);
+        console.log(questions);
+    } else {
+        alert("Bientôt disponible ! Pour l'instant, seules les catégories 'Provinces', 'Culture' et 'Géographie' ont des questions.");
+    }
+}
+
+function showPaywall() {
+    alert("Option VIP : Le paiement via Airtel/Moov Money arrive bientôt !");
+}
